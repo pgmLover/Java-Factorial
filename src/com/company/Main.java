3c4eb57514cb32fc8c54593e592a7362fff0dad1
pgmLover <anandakrishnapb11@gmail.com>
2022-04-18 11:54:24 +0530

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
+    static int Factorial(int n){
+        int fact =1;
+        for(int i=1; i<=n;i++){
+            fact=fact*i;
+        }
+        return (fact);
+    }
+
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the No For Factorial:");
+        int Num= sc.nextInt();
+        System.out.print("Factorial of "+Num+ " is: "+Factorial(Num));
+
     }
 }
